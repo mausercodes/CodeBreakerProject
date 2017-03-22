07f5b1deec11571801a4f9ce5eac29fb6a3cb97e
John Mauser <john_mauser09@hotmail.com>
2017-03-21 22:23:23 -0400

    comitted

--- assets/main.js
+++ assets/main.js
@@ -32,7 +32,7 @@ function setHiddenFields() {
 	attempt.value = 0;
 	answer.value = Math.floor(Math.random() * 10000).toString();
 
-	while(answer.length < 4){
+	while(answer.value.length < 4){
 		answer.value = '0' + answer.value;
 	}
 
